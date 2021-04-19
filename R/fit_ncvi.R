53e464214c492cd0880e8fc9647babeb9e82a6e1
dcannonwalker <42654459+dcannonwalker@users.noreply.github.com>
2021-04-19 15:31:57 -0500

    fix error in small errors

--- R/fit_ncvi.R
+++ R/fit_ncvi.R
@@ -3,40 +3,53 @@
 #' General form of wrapper function for a
 #' variational inference algorithm that may include
 #' both conjugate and non-conjugate conditional distributions.
+#'
 #' @param data List of observed or known variables used by the
-#' differentials or update functions.
+#'   differentials or update functions
 #' @param init List of initial values for unknown variables
-#' updated by the algorithm. Should have 'phi' and 'theta'.
-#' @param differentials List of functions for differentials used in
-#' non-conjugate updates of 'phi' parameters.
+#'   updated by the algorithm; should have 'phi' and 'theta'
 #' @param update_phi Function to carry out non-conjugate updates of
-#' 'phi' parameter. Should take args 'data', 'pars', and 'differentials'.
+#'   'phi' parameter
 #' @param update_theta Function to carry out conjugate updates of 'theta'
-#' parameter. Should take args 'data' and 'pars'.
-#' @param elbo Function to calculate the ELBO for the model.
+#'   parameter
+#' @param elbo Function to calculate the ELBO for the model
 #' @param options List of options.
-#' Should include positive real 'elbo_delta', the threshold
-#' change in ELBO to terminate the algorithm.
+#'   Should include positive real 'elbo_delta', the threshold
+#'   change in ELBO to terminate the algorithm
+#' @param ... Additional arguments to be passed to `update_phi()`,
+#'   e.g. `differentials`
 #' @export
 
 fit_ncvi <- function(data, init,
-                 differentials,
                  update_phi,
                  update_theta,
                  elbo,
                  options = list(max_iter = 100,
-                                elbo_delta = 0.001,
-                                verbose = T)) {
+                                elbo_delta = 0.0001,
+                                verbose = T),
+                 ...) {
+
+  args <- list(...)
   L = elbo(data, init)
   pars = init
   iter = 0
   delta = options$elbo_delta + 1
-  while (delta > options$elbo_delta) {
-    pars$phi <- update_phi(data, pars, differentials)
+
+
+  while (iter < options$max_iter &&
+         delta > options$elbo_delta) {
+
+    pars$phi <- update_phi(data, pars, args$differentials)
+
     pars$theta <- update_theta(data, pars)
+
     delta <- abs(L - (L <- elbo(data,pars)))
+
     iter <- iter + 1
-    if (verbose == T) print(c(L, iter))
+
+    if (options$verbose == T) print(c(L, iter))
+
   }
+
   pars
 }
--- R/nc_update_mvn.R
+++ R/nc_update_mvn.R
@@ -8,11 +8,11 @@
 #' @param differentials List of differentials functions, should have
 #' 'mu' and 'Sigma'
 #' @export
-
 nc_update_mvn <- function(data, pars, differentials) {
   Sigma <- MASS::ginv(
-    -2*differential_Sigma(data, pars)
+    -2*differentials$Sigma(data, pars)
     )
+  pars$Sigma <- Sigma
   mu <- pars$phi$mu + Sigma%*%differentials$mu(data, pars)
   list(mu = mu, Sigma = Sigma)
 }
