c5e6915549c13ab465a7c289d5e29bc8b6b31562
dcannonwalker <42654459+dcannonwalker@users.noreply.github.com>
2021-08-06 10:12:54 -0500

    Add functions for preparing real data; add 'ncvi.R' for package doc

---
+++ R/ncvi.R
@@ -0,0 +1,10 @@
+#' ncvi: a package for fitting Bayesian GLMM to RNA-Seq and Ribo-Seq data
+#' using non-conjugate variational methods.
+#'
+#'@section Prep functions
+#'
+#' @docType package
+#' @name ncvi
+#'
+#' @import dplyr
+NULL
