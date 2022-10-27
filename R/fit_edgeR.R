ae6cadb06d4896f79ae2c7b2ab73699c068d5118
dcannonwalker <42654459+dcannonwalker@users.noreply.github.com>
2022-10-27 08:19:24 -0500

    Remove unused versions of `fit_edgeR()` function

--- R/fit_edgeR.R
+++ R/fit_edgeR.R
@@ -1,31 +1,31 @@
-fit_edgeR <- function(data_edgeR) {
-  y <- edgeR::DGEList(counts = data_edgeR$counts,
-                      group = data_edgeR$group)
-  y <- edgeR::calcNormFactors(y)
-
-  y <- edgeR::estimateDisp(y, design = data_edgeR$design)
-
-  fit <- edgeR::glmFit(y, design = data_edgeR$design)
-  lrt <- edgeR::glmLRT(fit)
-  qobj <- qvalue::qvalue(lrt$table$PValue)
-  q <- qobj$qvalues
-  list(fit = fit, lrt = lrt, type_str = "edgeR",
-       true_null = data_edgeR$true_null,
-       p = p.adjust(lrt$table$PValue), q = q)
-}
-
-fit_edgeRV <- function(data_edgeR) {
-  y <- edgeR::DGEList(counts = data_edgeR$counts,
-                      group = data_edgeR$group)
-  y <- edgeR::calcNormFactors(y)
-
-  y <- edgeR::estimateCommonDisp(y)
-  tests <- edgeR::exactTest(y)
-  p <- tests$table$PValue
-  padj <- p.adjust(p, method="BH")
-
-  list(partFit = y, tests = tests, p = p, padj = padj)
-}
+# fit_edgeR <- function(data_edgeR) {
+#   y <- edgeR::DGEList(counts = data_edgeR$counts,
+#                       group = data_edgeR$group)
+#   y <- edgeR::calcNormFactors(y)
+#
+#   y <- edgeR::estimateDisp(y, design = data_edgeR$design)
+#
+#   fit <- edgeR::glmFit(y, design = data_edgeR$design)
+#   lrt <- edgeR::glmLRT(fit)
+#   qobj <- qvalue::qvalue(lrt$table$PValue)
+#   q <- qobj$qvalues
+#   list(fit = fit, lrt = lrt, type_str = "edgeR",
+#        true_null = data_edgeR$true_null,
+#        p = p.adjust(lrt$table$PValue), q = q)
+# }
+#
+# fit_edgeRV <- function(data_edgeR) {
+#   y <- edgeR::DGEList(counts = data_edgeR$counts,
+#                       group = data_edgeR$group)
+#   y <- edgeR::calcNormFactors(y)
+#
+#   y <- edgeR::estimateCommonDisp(y)
+#   tests <- edgeR::exactTest(y)
+#   p <- tests$table$PValue
+#   padj <- p.adjust(p, method="BH")
+#
+#   list(partFit = y, tests = tests, p = p, padj = padj)
+# }
 
 fit_edgeRribo <- function(data_edgeR, design.re) {
   y <- edgeR::DGEList(counts = data_edgeR$counts,
@@ -33,7 +33,7 @@ fit_edgeRribo <- function(data_edgeR, design.re) {
   y <- edgeR::calcNormFactors(y)
   y <- edgeR::estimateDisp(y, design = design.re)
   fit <- edgeR::glmQLFit(y, design = design.re)
-  test <- edgeR::glmQLFTest(fit, coef = 10)
+  test <- edgeR::glmQLFTest(fit, coef = ncol(design.re))
 
   list(table = test$table, p = test$table$PValue,
        padj = p.adjust(test$table$PValue, method = "BH"))
